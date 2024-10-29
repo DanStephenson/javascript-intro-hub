b04c38782d5170a6271c1e0d055125e08458c3f4
Dan Stephenson <dan.stephenson@sait.ca>
2024-10-29 16:08:21 -0600

    Four Seasons of Web Development Added
    The Four Seasons of Web Development Take it Further exercise has been added.

---
+++ take-it-further/four-seasons/seasons.js
@@ -0,0 +1,32 @@
+autumn.addEventListener('click',function(){
+	pagetitle.innerHTML = "Autumn";
+	console.log("autumn");
+	document.body.classList.remove(['spring','summer','winter']);
+	document.body.classList.add('autumn');
+	document.getElementById('banner').src="autumn.webp";
+	//document.getElementById('lightbulb').src = "lightbulb_on.webp";
+})
+
+spring.addEventListener('click',function(){
+	pagetitle.innerHTML = "Spring";
+	console.log("spring");
+	document.body.classList.remove(['winter','summer','autumn']);
+	document.body.classList.add('spring');
+	document.getElementById('banner').src="spring.webp";
+})
+
+summer.addEventListener('click',function(){
+	pagetitle.innerHTML = "Summer";
+	console.log("summer");
+	document.body.classList.remove(['spring','autumn','winter']);
+	document.body.classList.add('summer');
+	document.getElementById('banner').src="summer.webp";
+})
+winter.addEventListener('click',function(){
+	pagetitle.innerHTML = "Winter";
+	console.log("winter");
+	document.body.classList.remove(['spring','summer','autumn']);
+	document.body.classList.add('winter');
+	document.getElementById('banner').src="winter.webp";
+	
+})
