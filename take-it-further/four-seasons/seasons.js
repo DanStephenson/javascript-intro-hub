5f885042235b62e836268183c2cb4b8cf5e3bd53
Dan Stephenson <dan.stephenson@sait.ca>
2024-10-30 09:55:04 -0600

    Update seasons.js
    Fixed an issue with removing classes.

--- take-it-further/four-seasons/seasons.js
+++ take-it-further/four-seasons/seasons.js
@@ -1,7 +1,9 @@
 autumn.addEventListener('click',function(){
 	pagetitle.innerHTML = "Autumn";
 	console.log("autumn");
-	document.body.classList.remove(['spring','summer','winter']);
+	document.body.classList.remove("spring");
+	document.body.classList.remove("summer");
+	document.body.classList.remove("winter");
 	document.body.classList.add('autumn');
 	document.getElementById('banner').src="autumn.webp";
 	//document.getElementById('lightbulb').src = "lightbulb_on.webp";
@@ -10,7 +12,9 @@ autumn.addEventListener('click',function(){
 spring.addEventListener('click',function(){
 	pagetitle.innerHTML = "Spring";
 	console.log("spring");
-	document.body.classList.remove(['winter','summer','autumn']);
+	document.body.classList.remove("autumn");
+	document.body.classList.remove("summer");
+	document.body.classList.remove("winter");
 	document.body.classList.add('spring');
 	document.getElementById('banner').src="spring.webp";
 })
@@ -18,14 +22,18 @@ spring.addEventListener('click',function(){
 summer.addEventListener('click',function(){
 	pagetitle.innerHTML = "Summer";
 	console.log("summer");
-	document.body.classList.remove(['spring','autumn','winter']);
+	document.body.classList.remove("spring");
+	document.body.classList.remove("autumn");
+	document.body.classList.remove("winter");
 	document.body.classList.add('summer');
 	document.getElementById('banner').src="summer.webp";
 })
 winter.addEventListener('click',function(){
 	pagetitle.innerHTML = "Winter";
 	console.log("winter");
-	document.body.classList.remove(['spring','summer','autumn']);
+	document.body.classList.remove("spring");
+	document.body.classList.remove("summer");
+	document.body.classList.remove("autumn");
 	document.body.classList.add('winter');
 	document.getElementById('banner').src="winter.webp";
 	
